dd5f24d338a3c5c0064c6227872d75b279b26752
Jemomo123 <blessedmosomi@gmail.com>
2026-08-02 23:18:05 +0300

    Create Config.py for personal watchlist
    Added a personal watchlist configuration with various cryptocurrencies.

---
+++ Config.py
@@ -0,0 +1,32 @@
+# config.py
+# =====================================================================
+# PERSONAL WATCHLIST CONFIGURATION
+# =====================================================================
+
+WATCHLIST = [
+    "BTCUSDT",
+    "ETHUSDT",
+    "SOLUSDT",
+    "PEPEUSDT",
+    "BONKUSDT",
+    "SHIBUSDT",
+    "USELESSUSDT",
+    "SPACEUSDT",
+    "MOVEUSDT",
+    "ZECUSDT",
+    "SPXUSDT",
+    "PEOPLEUSDT",
+    "PENGUUSDT",
+    "FARTCOINUSDT",
+    "LINEAUSDT",
+    "MEMEUSDT",
+    "PUMPUSDT",
+    "AIXBTUSDT",
+    "BRETTUSDT",
+    "FOGOUSDT",
+    "GOOGLUSDT",
+    "FLOKIUSDT",
+    "IWMUSDT",
+    "MOODENGUSDT",
+    "NEARUSDT"
+]
